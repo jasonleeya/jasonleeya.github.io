7303923119d704926bd4357b36411c97815f980a
JasonLee <1615685977@qq.com>
2023-09-13 01:05:13 +0800

    2023年9月12日 01点05分

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -65,7 +65,7 @@ export default hopeTheme(
     comment: {
       // You should generate and use your own comment service
       provider: "Waline",
-      serverURL: "https://blog-kowxnwntg-jasonleeya.vercel.app",
+      serverURL: "https://waline.lsj97.com",
     },
 
     // all features are enabled for demo, only preserve features you need here
